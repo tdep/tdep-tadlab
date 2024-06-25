f54b8bd60061653982115703072601afa685a4ba
tdep <t.depewmusic@gmail.com>
2024-06-25 17:06:18 -0400

    adding base styling

--- tdeptadlabblog/tdeptadlabblog/settings.py
+++ tdeptadlabblog/tdeptadlabblog/settings.py
@@ -16,7 +16,7 @@ import os
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -63,7 +63,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'blog/templates')
+            os.path.join(BASE_DIR, 'templates/')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -128,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
